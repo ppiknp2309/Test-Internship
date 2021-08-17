586851789fb6c4fe5e6e65662e5a5363108f604d
ppiknp2309 <88672610+ppiknp2309@users.noreply.github.com>
2021-08-17 21:48:26 +0700

    Add files via upload

--- internship/10.py
+++ internship/10.py
@@ -1,8 +1,7 @@
 num = int(input("input : "))
-for i in range(num,0,-1):
-   for j in range(i,num):
+for i in range(num):
+    for j in range(num-i-1):
        print(" ",end="")
-   for k in range(1,(2*i-1)+1):
-       print("*",end="")
-   print()
-  
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
