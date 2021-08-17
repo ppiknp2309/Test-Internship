15e6bacfcb2ed9be30da796dff60c44452276e74
ppiknp2309 <88672610+ppiknp2309@users.noreply.github.com>
2021-08-17 21:47:57 +0700

    Add files via upload

--- internship/8.py
+++ internship/8.py
@@ -9,8 +9,6 @@ onebill = change //100
 change = change % 100
 fifbill = change // 50
 change = change % 50
-twenty = change //20
-change = change % 20
 tencoin = change //10
 change = change%10
 fivecoin = change // 5
@@ -21,7 +19,6 @@ change = change % 1
 print("ธนบัตร 500 ",fivebill,"ใบ")
 print("ธนบัตร 100 ",onebill,"ใบ")
 print("ธนบัตร 50 ",fifbill,"ใบ")
-print("ธนบัตร 20 ",twenty,"ใบ")
 print("เหรียญ 10 ",tencoin,"เหรียญ")
 print("เหรียญ 5 ",fivecoin,"เหรียญ")
 print("เหรียญ 1 ",onecoin,"เหรียญ")
